c1ebdce657d1d559e3dda7f12fda9fd1ea2a5119
Manki <2380444013@qq.com>
2024-08-16 22:29:49 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -8,11 +8,11 @@ from app.utils.config_directory import get_ddnet_directory
 base_path = os.path.dirname(__file__)
 
 class Config(QConfig):
-    themeMode = OptionsConfigItem("个性化", "ThemeMode", Theme.AUTO, OptionsValidator(Theme),
+    themeMode = OptionsConfigItem("Theme", "ThemeMode", Theme.AUTO, OptionsValidator(Theme),
                                   EnumSerializer(Theme))
     dpiScale = OptionsConfigItem(
-        "个性化", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]), restart=True)
-    themeColor = ColorConfigItem("个性化", "ThemeColor", "#009faa")
+        "Theme", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]), restart=True)
+    themeColor = ColorConfigItem("Theme", "ThemeColor", "#009faa")
     DDNetFolder = ConfigItem("DDNet", "DDNetFolder", get_ddnet_directory(), FolderValidator())
     DDNetCheckUpdate = ConfigItem("DDNet", "DDNetCheckUpdate", True, BoolValidator())
     DDNetAssetsCursor = ConfigItem("DDNet", "DDNetAssetsCursor", None)
